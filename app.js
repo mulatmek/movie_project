b62f375a62515fcebb1dede7bbc335b679d39e9b
avivs <avivshemesh8@gmail.com>
2022-06-07 22:44:00 +0300

    pushing for mulat

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser= require("body-parser");
 const { json } = require("express/lib/response");
 const mongoose = require("mongoose");
 const path = require("path");
+const imdb = require('imdb-api')
+const apiKey = "cab8d7cf"
 var cors = require("cors");
 var request = require("request");
 
@@ -15,6 +17,8 @@ app.engine("html", require("ejs").renderFile);
 
 connectionString = "mongodb+srv://avivs:AvivS123@cluster0.xuj1qss.mongodb.net/DB";
 mongoose.connect(connectionString, {useNewUrlParser:true});
+
+// imdb.get({name: "The Toxic Avenger"}, {apiKey: apiKey, timeout: 30000}).then(console.log).catch(console.log);
 
 const MovieSchema = {
     title: {type:String, unique: true},
@@ -49,17 +53,23 @@ app.post("/add_movie", function(req, res) {
 });
 
 app.post("/search", function(req, res) {
-    var url = "https://www.omdbapi.com/?s="+req.body.search+"&apikey=cab8d7cf";
-    request(url, function(error, response, body){
-            if (!error && response.statusCode == 200) {
-                // console.log(body);
-                var data = JSON.parse(body);
-                res.render(path.join(__dirname, "views", "results.html"), {data: data});
-            }
-    });
+    // var data = imdb.search({name: req.body.search}, {apiKey: apiKey});
+    imdb.search({name: req.body.search}, {apiKey: apiKey}).then(console.log).then(data =>
+        res.render(path.join(__dirname, "views", "results.html"), {data: data.json()})
+    );
 });
+// app.post("/search", function(req, res) {
+//     var url = "https://www.omdbapi.com/?s="+req.body.search+"&apikey=cab8d7cf";
+//     request(url, function(error, response, body){
+//             if (!error && response.statusCode == 200) {
+//                 // console.log(body);
+//                 var data = JSON.parse(body);
+//                 res.render(path.join(__dirname, "views", "results.html"), {data: data});
+//             }
+//     });
 
-app.get("/home", (req, res)=> {
+
+app.get("/home", (req, res) => {
     Movie.find({}, function(err, moviesArray) {
         if (!err) {
            res.json(moviesArray);
@@ -70,11 +80,11 @@ app.get("/home", (req, res)=> {
 
 
 //NOTE this should be the last get function
-app.get("*", function(req, res){
+app.get("*", function(req, res) {
     res.render(path.join(__dirname, "views", "error.html"));
 });
 
 var port = 8080;
 app.listen(port, function() {
-    console.log("Server listening on port " + 8080);
+    console.log("Server listening on port " + port);
 });
