a1af519cb8501520f394ffc9fed8a6747518153f
mulat <mulatmek@gmail.com>
2022-08-24 17:01:02 +0300

    add facebook page to about us

---
+++ client/src/views/AboutUs.js
@@ -0,0 +1,12 @@
+import { FacebookProvider, Like } from 'react-facebook';
+
+const AboutUs = () => {
+    return(
+        <body className="about" >
+            {/* <FacebookProvider appId="123456789">
+                <Like href="http://https://www.facebook.com/Movie-Center-100670486114477.facebook.com" colorScheme="dark" showFaces share />
+            </FacebookProvider> */}
+        </body>
+    );
+}
+export default AboutUs;
