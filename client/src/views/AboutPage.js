70ef4a1e4a0ea3a6e6044201c8f99b43f1c169f7
stav3434 <stav3434@gmail.com>
2022-08-25 12:52:34 +0300

    add the abous us page

---
+++ client/src/views/AboutPage.js
@@ -0,0 +1,16 @@
+
+const AboutPage = ()=>{
+    return(
+<body className="about-page">
+  <div class="about-information">
+At Movie-center, we want to entertain the world. Whatever your taste,
+ and no matter where you live, we give you access to best-in-class TV series, documentaries, feature films.
+  Since we launched in august 2022 our members control what they want to watch, when they want it, with no ads,
+  they also can download and watch movies and series with no internet, in one simple subscription.
+  because great stories can come from anywhere and be loved everywhere. 
+  We are the world’s biggest fans of entertainment, and we’re always looking to help you find your next favorite story.
+  </div>
+</body>
+);
+};
+export default AboutPage;
